658a7aba7e26f3ec3ed047b44fad1692632b36c1
Swaroop SM <swaroop.striker@gmail.com>
2012-08-26 11:55:34 +0530

    added private variables and a create function

--- functions/class.entries.php
+++ functions/class.entries.php
@@ -1,5 +1,12 @@
 <?php
 	class entries{
 		
+		private $id,$name,$college,$status;
+		
+		public function create($stu_name,$stu_college){
+			$stu_name=mysql_real_escape_string($stu_name);
+			$stu_college=mysql_real_escape_string($stu_college);
+			return(mysql_query("INSERT INTO entries(name,college) VALUES('$stu_name','$stu_college')"));
+		}
 	}
 ?>
