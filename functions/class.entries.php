31798d4a6ce3ed1d0a983e0304932f9f7f8876f8
Swaroop SM <swaroop.striker@gmail.com>
2012-08-26 13:36:58 +0530

    insert values only if names are not null

--- functions/class.entries.php
+++ functions/class.entries.php
@@ -8,7 +8,8 @@
 			$c=count($stu_name);
 			$q="INSERT INTO entries(name,college) VALUES ";
 			for($i=0;$i<$c;$i++){
-				$q=$q."('$stu_college','$stu_name[$i]'),";
+				if(trim($stu_name[$i]!=''))
+					$q=$q."('$stu_college','$stu_name[$i]'),";
 			}
 			$q=substr($q,0,-1);
 			if(mysql_query($q))
