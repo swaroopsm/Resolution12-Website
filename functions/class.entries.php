c7e4047404ff556fe9e6e9c246880fa455066caf
Swaroop SM <swaroop.striker@gmail.com>
2012-08-26 15:22:32 +0530

    view_json function that jsonifys the names of participants of a particular college

--- functions/class.entries.php
+++ functions/class.entries.php
@@ -10,13 +10,22 @@
 			for($i=0;$i<$c;$i++){
 				if(trim($stu_name[$i]!='')){
 					$a=mysql_real_escape_string($stu_name[$i]);
-					$q=$q."('$stu_college','$a'),";
+					$q=$q."('$a','$stu_college'),";
 				}
 			}
 			$q=substr($q,0,-1);
 			if(mysql_query($q))
 				return true;
 			return false;
+		}
+		
+		public function view_json($college){
+			$q=mysql_query("SELECT name FROM entries WHERE college='$college'");
+			$rows=array();
+			$values=array();
+			while($values=mysql_fetch_assoc($q))
+				$rows[]=$values;
+			echo json_encode($rows);
 		}
 	}
 ?>
