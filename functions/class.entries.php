0972144fcdfb46c9c042cfecf3ae6f4d3402cd06
Swaroop SM <swaroop.striker@gmail.com>
2012-08-26 13:43:33 +0530

    used mysql_real_escape_string function to escape splecial characters

--- functions/class.entries.php
+++ functions/class.entries.php
@@ -8,8 +8,10 @@
 			$c=count($stu_name);
 			$q="INSERT INTO entries(name,college) VALUES ";
 			for($i=0;$i<$c;$i++){
-				if(trim($stu_name[$i]!=''))
-					$q=$q."('$stu_college','$stu_name[$i]'),";
+				if(trim($stu_name[$i]!='')){
+					$a=mysql_real_escape_string($stu_name[$i]);
+					$q=$q."('$stu_college','$a'),";
+				}
 			}
 			$q=substr($q,0,-1);
 			if(mysql_query($q))
