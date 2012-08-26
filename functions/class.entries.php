3d1231e4f6a8157f89e4824ba9983a52ab7b4639
Swaroop SM <swaroop.striker@gmail.com>
2012-08-26 13:31:52 +0530

    insert values of students and colleges

--- functions/class.entries.php
+++ functions/class.entries.php
@@ -3,10 +3,17 @@
 		
 		private $id,$name,$college,$status;
 		
-		public function create($stu_name,$stu_college){
-			$stu_name=mysql_real_escape_string($stu_name);
+		public function create($stu_college,$stu_name){
 			$stu_college=mysql_real_escape_string($stu_college);
-			return(mysql_query("INSERT INTO entries(name,college) VALUES('$stu_name','$stu_college')"));
+			$c=count($stu_name);
+			$q="INSERT INTO entries(name,college) VALUES ";
+			for($i=0;$i<$c;$i++){
+				$q=$q."('$stu_college','$stu_name[$i]'),";
+			}
+			$q=substr($q,0,-1);
+			if(mysql_query($q))
+				return true;
+			return false;
 		}
 	}
 ?>
